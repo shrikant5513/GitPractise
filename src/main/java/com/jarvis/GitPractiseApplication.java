b3c17ca2845ea74f28f27de8c0c59e78a4b86c2a
shrikant5513 <shrikantbangonde5513@gmail.com>
2020-11-21 10:27:28 +0530

    project added to repo

---
+++ src/main/java/com/jarvis/GitPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.jarvis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPractiseApplication.class, args);
+	}
+
+}
